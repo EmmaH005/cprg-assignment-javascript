b04d61b53d9603c1cded51d77c46aff02afdac22
EmmaH005 <emma.hemphill@edu.sait.ca>
2025-03-27 17:41:36 -0600

    change.javascript
    modify javascript to debug

--- script.js
+++ script.js
@@ -3,21 +3,41 @@
 async function fetchJoke(category) {
     const apiUrl = `https://v2.jokeapi.dev/joke/${category}?safe-mode`;
 
-   
-    const response = await fetch(apiUrl);
-    const data = await response.json();
-
+    const response = await fetch(apiUrl); 
+    const data = await response.json();  
     const jokeContainer = document.getElementById('joke');
 
-   
+    
     if (data.type === "single") {
         jokeContainer.innerText = data.joke;
     } else if (data.type === "twopart") {
-        jokeContainer.innerText = `${data.setup} ... ${data.delivery}`;
-    } else {
-        jokeContainer.innerText = "Couldn't fetch a joke. Try again!";
+        jokeContainer.innerText = data.setup + " ... " + data.delivery; 
     }
 }
+
+// 
+fetchJoke('Any');
+
+
+document.getElementById("programmingBtn").addEventListener("click", function() {
+    fetchJoke('Programming');
+});
+
+document.getElementById("punBtn").addEventListener("click", function() {
+    fetchJoke('Pun');
+});
+
+document.getElementById("spookyBtn").addEventListener("click", function() {
+    fetchJoke('Spooky');
+});
+
+document.getElementById("christmasBtn").addEventListener("click", function() {
+    fetchJoke('Christmas');
+});
+
+document.getElementById("miscBtn").addEventListener("click", function() {
+    fetchJoke('Misc');
+});
 
 fetchJoke('Any');
 
